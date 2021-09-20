eec120b2fcaca4fbe23588496a84ca5e556759d5
kLamda <krishnakumar76068@gmail.com>
2021-09-20 12:23:52 +0530

    login/signup page added raw

--- PortRates/App.js
+++ PortRates/App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 import StartPage from './app/start';
+import Signup from './app/signup';
 import EndPage from './app/results';
 import Try from './app/result2';
+import Login from './app/login';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 
@@ -13,7 +15,31 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName="Signup"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#3740FE',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+        >
+        <Stack.Screen 
+          name="Signup" 
+          component={Signup} 
+          options={{ title: 'Signup' }}
+        />   
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+          options={
+            {title: 'Login'}
+          }
+        />
         <Stack.Screen name="try" component={Try} options={{ headerShown: false }}/>
         <Stack.Screen name="Start" component={StartPage} options={{ headerShown: false }} />
         <Stack.Screen name="Result" component={EndPage} />
